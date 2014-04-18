8ae9879ba0ecfaa9d5cf985df2e8d697c48c378f
Ali Abou-Zahr & Nolan Miller <student@epicodus.com>
2014-04-18 16:19:06 -0700

    retrieve tweets from the twitter api

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,14 @@ class WelcomeController < ApplicationController
   require 'twitter'
 
   def index
-    @users = User.all
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key = 'sINjDitlNubm6ZflEf4clUgor'
+      config.consumer_secret = 'ue9WpZNpAiSh2F98STn9AebXNInMTNbHJvbhHrGvsiBDorx3p9'
+      config.access_token = '2451963320-WLrz17FIBX7OaIiL8oKFB9K4QIdMQEps5qcmgtr'
+      config.access_token_secret = 'rMK61AaFGGzulutaEfj8GptIU0Tp6WPmzWbIRxs4ZokvO'
     end
 
-    # @nba = client.search('nba').first
-
+    @nba = client.search('nba').first
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SportsBlog::Application.routes.draw do
   root to: "welcome#index"
   devise_for :users
-  resources :posts
   resources :users
+  resources :posts
 end
